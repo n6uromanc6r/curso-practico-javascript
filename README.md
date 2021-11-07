5a06339579fcdc023fc9d63d5a8e964e66faddd7
Ne6uromanc6r <estadistancia@gmail.com>
2021-11-07 00:26:52 -0400

    Se añadieron archivos html y js, se creo el calculo del cuadrado

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # curso-practico-javascript
-Este curso es de experimentación con js siguiendo el curso de platzi
+## TALLER #1: FIGURAS GEOMÉTRICAS
+- Primer paso: Definir las fórmulas
+- Segundo paso: Implementar fórmulas en JS
+- Tercer paso: Crear funciones
+- Cuarto paso: Integrar JS con HTML
+
---
+++ figuras.js
@@ -0,0 +1,10 @@
+//calcular cuadrado
+
+const ladoCuadrado = 5;
+console.log("Los lados del cuadrado miden " + ladoCuadrado);
+const perimetroCuadrado = ladoCuadrado * 4
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log(`El perímetro es: ${perimetroCuadrado} cm`);
+
+console.log(`El área es: ${areaCuadrado} cm2`);
